65090d8353df2bf2b3fc1d8fe09f7db310321394
Ibraguim Albakov <ibraguimalbakov@macbook-air-de-ibraguim.home>
2023-02-28 00:07:32 +0100

    2_home

---
+++ ccp_00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctype.h>
+
+int main(int ac, char **av) 
+{
+    int i = 1;
+    int j = 0;
+
+    if (ac != 1)
+    {
+        while (i < ac)
+        {
+            j = 0;
+            while(av[i][j])
+                    std::cout << (char)std::toupper(av[i][j++]);
+            i++;
+        }
+        std::cout << std::endl;
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (0);
+}
