706a28d57d88c33e41edcd79247854d5533328a9
Ibraguim Albakov <ibraguimalbakov@macbook-air-de-ibraguim.home>
2023-03-19 21:33:31 +0100

    home_21

--- ccp_01/ex05/Harl.cpp
+++ ccp_01/ex05/Harl.cpp
@@ -6,34 +6,26 @@ Harl::~Harl(){}
 
 void	Harl::debug(void)
 {
-	std::cout << "[ DEBUG ]" << "\n";
-	std::cout << "I love having extra bacon for my";
-	std::cout << " 7XL-double-cheese-triple-pickle-";
-	std::cout << "specialketchup burger. I really do!" << std::endl;
+	std::cout << "!DEBUG!" << "\n";
+	std::cout << "I love having extra bacon" << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "[ INFO ]" << "\n";
-	std::cout << "I cannot believe adding extra bacon";
-	std::cout << " cost more money. You don’t put";
-	std::cout << " enough! If you did I would not";
-	std::cout << " have to ask for it!" << std::endl;
+	std::cout << "!INFO!" << "\n";
+	std::cout << "I cannot believe adding extra bacon" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "[ WARNING ]" << "\n";
-	std::cout << "I think I deserve to have some extra bacon";
-	std::cout << "for free. I’ve been coming for years whereas";
-	std::cout << " you started working here since last month." << std::endl;
+	std::cout << "!WARNING!" << "\n";
+	std::cout << "I think I deserve to have some extra bacon" << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "[ ERROR ]" << "\n";
-	std::cout << "This is unacceptable, I want to speak to";
-	std::cout << " the manager now." << std::endl;
+	std::cout << "!ERROR!" << "\n";
+	std::cout << "This is unacceptable" << std::endl;
 }
 
 std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
